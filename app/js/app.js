529d54cae74d210daf46b14e689787c2094f860b
zencloud <mryanmill@gmail.com>
2019-06-29 21:40:28 -0600

    Profile Animation Stages

--- app/js/app.js
+++ app/js/app.js
@@ -7,14 +7,21 @@ const appData = {
 
 }
 
-$.ajax({
-    url: "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi?q=get%3Anew7-!1900,2018-!0,5-!0,10-!0-!Any-!Any-!Any-!gt100-!{downloadable}&t=ns&cl=all&st=adv&ob=Relevance&p=1&sa=and",
-    headers: {
-        'X-RapidAPI-Host':'unogs-unogs-v1.p.rapidapi.com',
-        'X-RapidAPI-Key':'4d121bd4f5mshafce35864f40836p1d896cjsn949371756abd'
-    },
 
-    success: function(response) {
-        console.log(response);
-    }
+
+// Document Ready
+$(document).ready(function () {
+
+    // Animate Main Profile
+    $('#app-stage-profile').fadeIn('slow');
+
+    // Set Listener on button
+    $('#btn-submit-profile').on('click', function () {
+        app_profile_submit_name();
+    });
+
+    // Set Listener on button
+    $('#btn-submit-profile-icon').on('click', function () {
+        app_profile_submit_icon();
+    });
 });
